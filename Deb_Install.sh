a1055fd4a1458d97803220e67a76d2452d4607c7
AtharvNatu <66716779+AtharvNatu@users.noreply.github.com>
2021-06-29 14:35:34 +0530

    Initial Commit

--- Deb_Install.sh
+++ Deb_Install.sh
@@ -1,3 +1,5 @@
 sudo apt install zsh zsh-syntax-highlighting
-touch "$HOME/.cache/zshhistory" #-- Setup Alias in $HOME/zsh/aliasrc git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+touch "$HOME/.cache/zshhistory" #-- Setup Alias in $HOME/zsh/aliasrc 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k 
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 chsh $USER
